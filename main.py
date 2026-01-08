97965b6efab23b7043f55ee2bc7cf878a88a447a
localman-git <efitton5491@gmail.com>
2026-01-08 20:42:33 +0000

    structure and logging, pre game loop

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from asteroids!")
+import pygame
 
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+def main():
+    print(f'Starting Asteroids with pygame version: {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}')
 
 if __name__ == "__main__":
     main()
