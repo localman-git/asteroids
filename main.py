fd51cf0df1d29ac1c1b860bef687ecfe4a0ec8ce
localman-git <efitton5491@gmail.com>
2026-01-08 22:00:42 +0000

    clock in, ready for sprites

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
 
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -23,6 +26,8 @@ def main():
 
         screen.fill('black')
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
