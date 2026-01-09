89da9155a6e76af29d1c5ef0ce020c3dc5217a34
localman-git <efitton5491@gmail.com>
2026-01-09 22:59:18 +0000

    player now on screen

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
 
@@ -15,6 +16,7 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
 
@@ -25,6 +27,7 @@ def main():
                 return
 
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
